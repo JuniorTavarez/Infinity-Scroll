b39bb6273ef9d42070a5daf32bdc7b6d6ae39aac
Junior Tavarez <junior.tavarez@crossroadsautogroup.local>
2024-05-03 12:51:23 -0400

    Added scroll functionality

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
-console.log('hello')
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = 'QNef9XpI4FP1-PXebPApOXwluf276eq1u0udd6twR9A';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper function To set Attribute on Dom Element
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Element For Links & Photos, Add To Dom
+function displayPhotos() {
+  // Run Functionfor each object in PhotosAray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // item.setAttribute('href', photo.links.html);
+    // item.setAttribute('target', '_blank');
+    // Create Image for photo
+    const img = document.createElement('img');
+
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // img.setAttribute('src', photo.urls.regular);
+
+    // img.setAttribute('alt', photo.alt_description);
+    // img.setAttribute('title', photo.alt_description);
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get photos from Unsplash API
+
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (err) {}
+}
+
+// On Load
+getPhotos();
